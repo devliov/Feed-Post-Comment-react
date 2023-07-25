c703fa7a7bab0b43db7ba889e68548463497eceb
Eli <eli.sousa.2002@gmail.com>
2023-07-24 22:09:53 -0300

    ci: base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
+  server: {
     port: 3001,
-  }
-})
+  },
+  base: "/Feed-Post-Comment-react",
+});
